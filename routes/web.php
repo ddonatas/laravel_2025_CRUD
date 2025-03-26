cff8dfedbb97aefd57658c2c4200536f8efae9f1
donatas <d.dervinis@svako.lt>
2025-03-26 16:00:09 +0200

    Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,48 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/*Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
+
 
 Route::post('students/{id}/restore', [StudentController::class, 'restore'])->name('students.restore');
 Route::delete('students/{id}/forceDelete', [StudentController::class, 'forceDelete'])->name('students.forceDelete');
-//Route::get('/students', [StudentController::class, 'index']);
 Route::resource('students', StudentController::class);
+*/
+
+
+
+
+// Studento sąrašas prieinamas visiems
+Route::get('students', [StudentController::class, 'index'])->name('students.index');
+
+// Visi kiti studentų maršrutai reikalauja autentifikacijos
+Route::middleware('auth')->group(function () {
+
+    Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
+    Route::post('students/{id}/restore', [StudentController::class, 'restore'])->name('students.restore');
+    Route::delete('students/{id}/forceDelete', [StudentController::class, 'forceDelete'])->name('students.forceDelete');
+    Route::resource('students', StudentController::class)->except(['index']);
+});
+
+
+
+
+require __DIR__.'/auth.php';
